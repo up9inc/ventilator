d4bb81729f55e577cf1c4ed6db4b8d136a7596a0
Selton Fiuza <seltonfiuza@gmail.com>
2021-03-23 13:50:20 -0300

    Adding to helper on args: Default

--- ventilator/args.py
+++ ventilator/args.py
@@ -3,12 +3,12 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-c", "--configurator",
-                    help="Web / CLI / File", default="CLI", action='store')
-parser.add_argument("-t", "--type_input", help="docker-compose / kubernetes",
+                    help="Web / CLI / File. Default: CLI", default="CLI", action='store')
+parser.add_argument("-t", "--type_input", help="docker-compose / kubernetes. Default: docker-compose",
                     default="docker-compose", action='store')
-parser.add_argument("-i", "--input", help="docker-compose file",
+parser.add_argument("-i", "--input", help="docker-compose / kubernetes file. Default: docker-compose.yml",
                     default="docker-compose.yml", action='store')
-parser.add_argument("-o", "--output", help="Ventilator Output Path",
+parser.add_argument("-o", "--output", help="Ventilator Output Path. Default: ./output",
                     default="./output", action='store')
 
 args = parser.parse_args()
